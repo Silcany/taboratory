30460f6dc5fa1d9ac3d21c504eedac5c39f9a942
yamanaka <contact@silva.vc>
2015-07-04 04:10:52 +0900

    ちょっと速度あげ

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,6 +2,10 @@ class IndexController < ApplicationController
   def index
     @voice = Voice.new
     @members = ExamGroup.where(name: ["計算工学", "通信情報工学", "知能システム科学"]).map(&:members).flatten.sample(9)
-    @groups = ExamGroup.joins(:members).group('exam_groups.id')
+    @groups = Member.group(:exam_group_id).count
+    @groups = ExamGroup.find_by_sql([
+      'SELECT exam_groups.*, COUNT(exam_groups.id) AS members_count FROM exam_groups
+       JOIN members on exam_groups.id = members.exam_group_id
+       GROUP BY exam_groups.id'])
   end
 end
