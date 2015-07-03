b866a47b5050f887cb385de772e96aff558ea136
yamanaka <contact@silva.vc>
2015-07-04 00:27:18 +0900

    トップにランダムにmemberページへのリンク

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def index
     @voice = Voice.new
-    @members = ExamGroup.find_by_name('計算工学').members
+    @members = ExamGroup.where(name: ["計算工学", "通信情報工学", "知能システム科学"]).map(&:members).flatten.sample(9)
   end
 end
