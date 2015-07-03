efcef43e9d7752733b6dc0bfb4449ac1e4684168
yamanaka <contact@silva.vc>
2015-07-04 07:46:06 +0900

    postgreにはRANDはないのであった

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,7 @@ class IndexController < ApplicationController
     @voice = Voice.new
     all_members_ids = Member.order_by_score
     members_ids = all_members_ids.first(3) + all_members_ids.reverse.first(3) +
-      Member.where(exam_group_id: ExamGroup.where(name: ["計算工学", "通信情報工学", "知能システム科学"]).pluck(:id)).order('RAND()').limit(3).pluck(:id)
+      Member.where(exam_group_id: ExamGroup.where(name: ["計算工学", "通信情報工学", "知能システム科学"]).pluck(:id)).pluck(:id).sample(3)
     @members = Member.includes(:thinks).where(id: members_ids.uniq)
 
     @groups = Member.group(:exam_group_id).count
